a4e89e2c32283d3f92838935f430885304d129fc
Samuel's Musical Library Official Website Repository <76181330+SCMcoding@users.noreply.github.com>
2022-10-07 21:17:01 -0400

    update

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,18 +1,50 @@
 using UnityEngine;
+//using UnityEngine.Input;
+using UnityEngine.InputSystem;
+
 using System.Collections;
 
 public class Bullet : MonoBehaviour
 { 
 public GameObject projectile;
+    public float projectileForce = 1000f;
 
-    void Update()
+    public void Onfire(InputAction.CallbackContext context)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            
-            GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
-            bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 10000);
 
-        }
+            //GameObject go = Instantiate(projectile);
+            GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+            bullet.GetComponent<Rigidbody>().AddForce(transform.forward);
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //public void OnFire(InputAction inputAction)
+
+
+
+    //void Update()
+    //{
+    //if (Input.GetKeyDown(KeyCode.Space))
+    //{
+
+    //    GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+    //    bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 10000);
+
+    //}
+    // }
 }
